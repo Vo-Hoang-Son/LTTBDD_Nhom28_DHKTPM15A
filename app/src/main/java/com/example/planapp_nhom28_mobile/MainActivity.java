6950b086f57a36f7a80006598e9092e6a90077de
Vo-Hoang-Son <19508571.iuh@gmail.com>
2022-05-10 10:09:05 +0700

    First commit

---
+++ app/src/main/java/com/example/planapp_nhom28_mobile/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.planapp_nhom28_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mloginemail,mloginpassword;
+    private RelativeLayout mlogin,mgotosignup;
+    private  TextView mgotoforgotpassword;
+
+    private FirebaseAuth firebaseAuth;
+
+    ProgressBar mprogressbarofmainactivity;
+    private static final int RC_SIGN_IN = 1;
+    private FirebaseAuth fAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient googleApiClient;
+    GoogleSignInAccount account;
+
+    private void signIn() {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        account = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+        //updateUI(account);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        mloginemail=findViewById(R.id.loginemail);
+        mloginpassword=findViewById(R.id.loginpassword);
+        mlogin=findViewById(R.id.login);
+        mgotoforgotpassword=findViewById(R.id.gotoforgotpassword);
+        mgotosignup=findViewById(R.id.gotosignup);
+        mprogressbarofmainactivity=findViewById(R.id.progressbarofmainactivity);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser!=null)
+        {
+            finish();
+            startActivity(new Intent(MainActivity.this, PlanActivity.class));
+        }
+
+
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, register.class));
+            }
+        });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, com.example.planapp_nhom28_mobile.fogotpassword.class));
+            }
+        });
+
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=mloginemail.getText().toString().trim();
+                String password=mloginpassword.getText().toString().trim();
+
+                if(mail.isEmpty()|| password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All Field Are Required",Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    // login the user
+
+
+                    mprogressbarofmainactivity.setVisibility(View.VISIBLE);
+
+                    firebaseAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                            if(task.isSuccessful())
+                            {
+                                checkmailverfication();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Account Doesn't Exist",Toast.LENGTH_SHORT).show();
+                                mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+                            }
+
+
+                        }
+                    });
+
+
+
+
+                }
+            }
+        });
+
+    }
+
+
+    private void checkmailverfication()
+    {
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser.isEmailVerified()==true)
+        {
+            Toast.makeText(getApplicationContext(),"Logged In",Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(MainActivity.this, PlanActivity.class));
+        }
+        else
+        {
+            mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),"Verify your mail first",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
+    }
+
+
+
+
+}
